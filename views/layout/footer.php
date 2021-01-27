57c1f7d622873abe5a415b64416d3850ad8add6e
acasadiegos <acasadiego199827@gmail.com>
2021-01-27 18:42:34 -0500

    Subida de proyecto Tienda Virtual

---
+++ views/layout/footer.php
@@ -0,0 +1,10 @@
+</div>
+</div>
+
+			<!-- PIE DE PÁGINA -->
+			<footer id="footer">
+				<p>Desarrollado por Andrés Casadiegos &copy; <?= date('Y') ?></p>
+			</footer>
+		</div>
+	</body>
+</html>
